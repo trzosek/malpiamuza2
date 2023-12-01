32d714b46a39c4a2c6a32d209e1c34770a3217c5
Wieczorkiewicz <126008604+trzosek@users.noreply.github.com>
2023-12-01 21:57:32 +0100

    Delete prisma/migrations directory

--- prisma/migrations/20231107152936_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "playlist" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "author" TEXT NOT NULL,
-    "cover" TEXT NOT NULL
-);
